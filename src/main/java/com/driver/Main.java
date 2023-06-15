6b9540c1ff9c18befdbb808b5b30fbc3164a389e
Chirag Goyal <97518994+Luffy6441@users.noreply.github.com>
2023-06-15 15:52:43 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-    
+    public static void main(String args[])
+    {
+    } 
+}
+
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B{
+    public String meth(){
+        return super.meth();
+    }
 }
